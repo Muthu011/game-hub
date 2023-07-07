2aeb45606bf08eb88789c860e4790bf8614b1716
Muthu <muthuvijayakumar5@gmail.com>
2023-07-07 14:17:02 +0530

    clean up game cards

--- src/components/GameCards/GameCards.tsx
+++ src/components/GameCards/GameCards.tsx
@@ -14,12 +14,13 @@ const GameCards = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
           <PlatformIcon
             platform={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
+
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
         </HStack>
       </CardBody>
     </Card>
--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -27,29 +28,37 @@ const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   }
 
   return (
-    <List>
-      {data.map((item) => (
-        <ListItem key={item.id} paddingY={"5px"}>
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(item.image_background)}
-              boxSize={"32px"}
-              borderRadius={8}
-            />
-            <Button
-              fontSize={"large"}
-              fontWeight={item.id === selectedGenre?.id ? "bold" : "normal"}
-              variant={"link"}
-              onClick={() => {
-                onSelectedGenre(item);
-              }}
-            >
-              {item.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize={"2xl"} marginY={5}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((item) => (
+          <ListItem key={item.id} paddingY={"5px"}>
+            <HStack>
+              <Image
+                objectFit={"cover"}
+                src={getCroppedImageUrl(item.image_background)}
+                boxSize={"32px"}
+                borderRadius={8}
+              />
+              <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                fontSize={"large"}
+                fontWeight={item.id === selectedGenre?.id ? "bold" : "normal"}
+                variant={"link"}
+                onClick={() => {
+                  onSelectedGenre(item);
+                }}
+              >
+                {item.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
